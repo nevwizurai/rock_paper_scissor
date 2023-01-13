8d7b6d4d7937b6be16cbf6fdcc10901e536ced31
Nevir Wizurai <nevwizurai@gmail.com>
2023-01-14 01:04:43 +0700

    added UI skeleton

--- script.js
+++ script.js
@@ -50,43 +50,10 @@ function playRound(playerChoice, computerChoice) {
   return { winner: winner, winChoice: winningChoice, loseChoice: losingChoice };
 }
 
-function game() {
-  // Setting up score variables
-  let playerScore = 0;
-  let computerScore = 0;
-
-  // Playing for 5 rounds
-  for (let i = 1; i <= 5; i++) {
-    let playerChoice = prompt(
-      `Round ${i}\nPlayer choice (rock, paper, scissor):`
-    ).toLowerCase();
-
-    let result = playRound(playerChoice, getComputerChoice());
-    switch (result.winner) {
-      case "Player":
-        playerScore++;
-        break;
-      case "Computer":
-        computerScore++;
-        break;
-    }
-    console.log(`---- Round ${i} --------------------------------`);
-    if (result.winner === "Tie")
-      console.log("It's a Tie! both side picked the same choice!");
-    else
-      console.log(
-        `${result.winner} wins! ${result.winChoice} beats ${result.loseChoice}!`
-      );
-    console.log(
-      `Current Score:\nPlayer Score: ${playerScore}\nComputer Score: ${computerScore}`
-    );
-    console.log("------------------------------------------------");
-  }
-
-  console.log("\n\n---- Final Result ------------------------");
-  console.log(`Player Score: ${playerScore}\nComputer Score: ${computerScore}`);
-  console.log("--------------------------------------------");
-  console.log("THANKS FOR PLAYING <3");
+function handleClick(playerChoice) {
+  console.log(playerChoice);
 }
 
-game();
+function handleLoad() {
+  console.log("loaded");
+}
